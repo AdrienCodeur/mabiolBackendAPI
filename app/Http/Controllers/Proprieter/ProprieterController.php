0fdf4d7a83f0c4d3a25ae1deee8288a7aff4e533
ivan01-tech <ivansilatsa@gmail.com>
2024-05-19 16:17:29 +0100

    rien de rien

--- app/Http/Controllers/Proprieter/ProprieterController.php
+++ app/Http/Controllers/Proprieter/ProprieterController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Proprieter;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\ProprierterRequest;
 use App\Models\Bien;
 use Carbon\Carbon;
 use Exception;
